625c373887d8f212909c66f6239df97a3eab54c5
Buster422 <benjamin@gofranks.com>
2019-09-16 19:21:09 -0400

    stupid

--- Program.cs
+++ Program.cs
@@ -12,9 +12,9 @@ namespace TextAdventure
         static void Main(string[] args)
         {
             Room StartingRoom = new Room(5, 5);
+
             StartingRoom.AddWall(0, 0).AddWall(1, 0).AddWall(2, 0).AddWall(3, 0).AddWall(4, 0).AddWall(4, 1).AddWall(4, 2).AddWall(0, 1).AddWall(0, 2);
-            Room NewRoom = new Room(5, 5);
-            StartingRoom.AddExit(4, 4, NewRoom, 0, 0);
+
             Player p = StartingRoom.AddPlayer(1, 1);
 
             p.GetInput(Console.OpenStandardInput());
